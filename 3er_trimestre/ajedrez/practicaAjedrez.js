ddbdd25e413d48789a1b19fcaec19653a70893b5
Elena-Alvarez-Campos <elenaalvarezcampos8@gmail.com>
2026-05-11 15:14:13 +0200

    incorporar practica ajedrez

--- 3er_trimestre/ajedrez/practicaAjedrez.js
+++ 3er_trimestre/ajedrez/practicaAjedrez.js
@@ -64,11 +64,25 @@ function tableroAjedrez(){
         tableroString+="\n"
     }
     console.log(tableroString)
-    //Comprobación fichas
+    
+    return(tablero)
+}
+let tablero=tableroAjedrez()
+
+
+function tipoPieza(casilla){
+    if(casilla==" "+"T"+"|"){return "Se puede comer la torre"}
+    else if(casilla==" "+"C"+"|"){return "Se puede comer el caballo"}
+    else if(casilla==" "+"A"+"|"){return "Se puede comer el alfil"}
+    else if(casilla==" "+"P"+"|"){return "Se puede comer el peón"}
+    else if(casilla==" "+"K"+"|"){return "Se puede comer el rey"}
+    else if(casilla==" "+"Q"+"|"){return "Se puede comer la reina"}
+    else{return "No puede comer ninguna pieza"}
+}
+function comePeon(requisito){
     for(let i=0;i<=8;i++){
         for(let j=0;j<=8;j++){
-            if(tablero[i][j]==" "+"P"+"|"){//PEÓN
-                console.log("Peón:")
+            if(tablero[i][j]==requisito){//PEÓN
                 if(tablero[i-1][j-1]!="  |"){
                     console.log(tipoPieza(tablero[i-1][j-1]))
                 }else if(tablero[i-1][j+1]!="  |"){
@@ -76,8 +90,14 @@ function tableroAjedrez(){
                 }else{
                     console.log("No puede comer ninguna pieza")
                 }
-            }else if(tablero[i][j]==" "+"T"+"|"){//TORRE
-                console.log("Torre:")
+            }
+        }
+    }
+}
+function comeTorre(requisito) {
+    for(let i=0;i<=8;i++){
+        for(let j=0;j<=8;j++){
+            if(tablero[i][j]==requisito){//TORRE
                 let encontrarT=false
                 //horizontal
                 for(let cadaCasilla of tablero[i]){
@@ -104,22 +124,115 @@ function tableroAjedrez(){
                 if(encontrarT==false){
                     console.log("No puede comer ninguna pieza")
                 }
-            }else if(tablero[i][j]==" "+"A"+"|"){
-                console.log("Alfil:")
-
             }
         }
     }
-    return(tablero)
 }
-tableroAjedrez()
-
-function tipoPieza(casilla){
-    if(casilla==" "+"T"+"|"){return "Se puede comer la torre"}
-    else if(casilla==" "+"C"+"|"){return "Se puede comer el caballo"}
-    else if(casilla==" "+"A"+"|"){return "Se puede comer el alfil"}
-    else if(casilla==" "+"P"+"|"){return "Se puede comer el peón"}
-    else if(casilla==" "+"K"+"|"){return "Se puede comer el rey"}
-    else if(casilla==" "+"Q"+"|"){return "Se puede comer la reina"}
-    else{return "No puede comer ninguna pieza"}
+function comeAlfil(requisito) {
+    for(let i=0;i<=8;i++){
+        for(let j=0;j<=8;j++){
+            if(tablero[i][j]==requisito){
+                let encontrar=false
+                //Parte de arriba v
+                for(let x=1;i-x>0;x++){
+                    if(tipoPieza(tablero[i-x][j-x])!="No puede comer ninguna pieza"){
+                        console.log(tipoPieza(tablero[i-x][j-x]))
+                        encontrar=true
+                    }if(tipoPieza(tablero[i-x][j+x])!="No puede comer ninguna pieza"){
+                        console.log(tipoPieza(tablero[i-x][j+x]))
+                        encontrar=true
+                    }
+                }
+                //Parte de abajo ^
+                for(let x=1;i+x<=8;x++){
+                    if(tipoPieza(tablero[i+x][j-x])!="No puede comer ninguna pieza"){
+                        console.log(tipoPieza(tablero[i+x][j-x]))
+                        encontrar=true
+                    }if(tipoPieza(tablero[i+x][j+x])!="No puede comer ninguna pieza"){
+                        console.log(tipoPieza(tablero[i+x][j+x]))
+                        encontrar=true
+                    }
+                }
+                if(encontrar==false){
+                    console.log("No puede comer ninguna pieza")
+                }
+            }
+        }
+    }
 }
+function comeCaballo(requisito) {
+    for(let i=0;i<=8;i++){
+        for(let j=0;j<=8;j++){
+            if(tablero[i][j]==requisito){
+                let encontrado=false;
+                if(tipoPieza(tablero[i+3][j+1])!="No puede comer ninguna pieza"){
+                    console.log(tipoPieza(tablero[i+3][j+1]))
+                    encontrado=true
+                }if(tipoPieza(tablero[i+3][j-1])!="No puede comer ninguna pieza"){
+                    console.log(tipoPieza(tablero[i+3][j-1]))
+                    encontrado=true
+                }if(tipoPieza(tablero[i-3][j+1])!="No puede comer ninguna pieza"){
+                    console.log(tipoPieza(tablero[i-3][j+1]))
+                    encontrado=true
+                }if(tipoPieza(tablero[i-3][j-1])!="No puede comer ninguna pieza"){
+                    console.log(tipoPieza(tablero[i-3][j-1]))
+                    encontrado=true
+                }if(encontrado==false){
+                    console.log("No puede comer ninguna pieza")
+                }
+            }
+        }
+    }
+}
+function comeReina(requisito) {
+    comeAlfil(requisito)
+    comeTorre(requisito)
+}
+function comeRey(requisito) {
+    for(let i=0;i<=8;i++){
+        for(let j=0;j<=8;j++){
+            if(tablero[i][j]==requisito){
+                let encontrado=false;
+                if(tablero[i-1][j-1]!="  |"){
+                    encontrado=true
+                    console.log(tipoPieza(tablero[i-1][j-1]))
+                }if(tablero[i-1][j+1]!="  |"){
+                    console.log(tipoPieza(tablero[i-1][j+1]))
+                    encontrado=true
+                }if(tablero[i+1][j-1]!="  |"){
+                    console.log(tipoPieza(tablero[i+1][j-1]))
+                    encontrado=true
+                }if(tablero[i+1][j+1]!="  |"){
+                    console.log(tipoPieza(tablero[i+1][j+1]))
+                    encontrado=true
+                }if(tablero[i-1][j]!="  |"){
+                    console.log(tipoPieza(tablero[i-1][j]))
+                    encontrado=true
+                }if(tablero[i+1][j]!="  |"){
+                    console.log(tipoPieza(tablero[i+1][j]))
+                    encontrado=true
+                }if(tablero[i][j+1]!="  |"){
+                    console.log(tipoPieza(tablero[i][j+1]))
+                    encontrado=true
+                }if(tablero[i][j-1]!="  |"){
+                    console.log(tipoPieza(tablero[i][j-1])) 
+                    encontrado=true   
+                }if(encontrado==false){
+                    console.log("No puede comer ninguna pieza")
+                }
+            }
+        }
+    }
+}
+console.log("Torre:")
+comeTorre(" "+"T"+"|")
+console.log("Peón:")
+comePeon(" "+"P"+"|")
+console.log("Alfil:")
+comeAlfil(" "+"A"+"|")
+console.log("Reina:")
+comeReina(" "+"Q"+"|")
+console.log("Rey:")
+comeRey(" "+"K"+"|")
+console.log("Caballo")
+comeCaballo(" "+"C"+"|")
--- 3er_trimestre/ajedrez/prueba.js
+++ 3er_trimestre/ajedrez/prueba.js
@@ -2,5 +2,7 @@ let tablero=[[],[]]
 tablero[0][0]="a"
 tablero[0][1]="b"
 tablero[1][0]="c"
-console.log(tablero)
+//console.log(tablero)
 tablero=[["A","B","C"],[1,2,3]]
+let prueba=[2,4]
+console.log(prueba[5])
