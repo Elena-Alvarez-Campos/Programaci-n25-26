400bbb81bf05b41242c93ab64ec028c0210b749e
Elena-Alvarez-Campos <elenaalvarezcampos8@gmail.com>
2026-05-26 19:11:01 +0200

    reversi sin movimientos ilegales

--- 3er_trimestre/Reversi/reversi.js
+++ 3er_trimestre/Reversi/reversi.js
@@ -9,7 +9,7 @@ const rl=readline.createInterface({input,output});
 //Crear tablero
 //i=columna
 //j=fila
-let tablero=[]
+let tablero=[]//se va a usar a lo largo del ejercicio
 for (let i=0;i<8;i++) {
     tablero[i]=[]//crea una fila
     for (let j=0;j<8;j++) {
@@ -45,12 +45,12 @@ function mostrar(){
 }
 mostrar()
 //Determina si la jugada es válida
-function turno(jugador,rival,letra,num) {
+function turno(jugador,rival,letra,num) {///////////////////////////////////////////////////////////////////////////////////////////////////////
     let i=num-1//numero
     let j=letras.indexOf(letra)//letra
     //console.log(''+i+' '+j)
     if(tablero[i][j]==null){
-        let casillaAntes=tablero[i][j]
+        let casillaAntes=tablero[i][j]//no puede ser null porque puede que el jugador haya introducido las coordenadas de otra ficha
         tablero[i][j]=jugador
         let rodear=buscaRival(i,j,rival,jugador)
         if(rodear==false){
@@ -68,63 +68,79 @@ function turno(jugador,rival,letra,num) {
 // arriba: i-1
 // derecha: j+1
 // izquierda: j-1
-function buscaRival(i,j,rival,jugador){//buscar si hay un rival en general
+function buscaRival(i,j,rival,jugador){//buscar si hay un rival en general////////////////////////////////////////////////////////////////////
     //i y j representan la posición de la pieza nueva
     
     let encontrado=false;
-    let validez
+    let validez=false;
     try{
         if(tablero[i-1][j]==rival){//ariba
-            volteaFicha(i-1,j,rival,jugador,-1,0)
+            if(volteaFicha(i-1,j,rival,jugador,-1,0)==true){
+                validez=true
+            }
             encontrado=true
         }
     }catch(error){}
     try {
         if(tablero[i][j-1]==rival){//izquierda
-            volteaFicha(i,j-1,rival,jugador,0,-1)
+            if(volteaFicha(i,j-1,rival,jugador,0,-1)==true){
+                validez=true
+            }
             encontrado=true
         }
     } catch (error) {}
     try {
         if(tablero[i][j+1]==rival){//derecha
-            volteaFicha(i,j+1,rival,jugador,0,1)
+            if(volteaFicha(i,j+1,rival,jugador,0,1)==true){
+                validez=true
+            }
             encontrado=true
         }
     } catch (error) {}
     try {
         if(tablero[i+1][j]==rival){//abajo
-            volteaFicha(i+1,j,rival,jugador,1,0)
+            if(volteaFicha(i+1,j,rival,jugador,1,0)==true){
+                validez=true
+            }
             encontrado=true
         }
     } catch (error) {}
     
     if(encontrado==false){
         return false
-    }else{
+    }else if(validez==true){
         return true
+    }else{
+        return false
     }
 }
-function volteaFicha(i,j,rival,jugador,suma_i,suma_j){
-    let x=i
-    let y=j
-    let voltear=false
-    for (let z = 0; z < 2; z++){
-        i=x
-        j=y
+function volteaFicha(i,j,rival,jugador,suma_i,suma_j){////////////////////////////////////////////////////////////////////////////////////////
+    let x=i//posición inicial de la ficha qeu se va a querer voltear
+    let y=j// " " " " 
+    let voltear=false//determina si se van a voltear las fichas
+    for (let z = 0; z < 2; z++){//se recorre 2 veces: 1 pasa determinar si se voltean las fichas y otra para voltearlas
+        i=x//posición inicial
+        j=y// " " " "
         if(voltear==false && z==1){//no hace falta que vuelva a recorrer el bucle si no hay ninguna ficha del jugador
+            //es decir, si no hay que voltear nada no recorre nada
             break
         }
         while (i>=0&&i<tablero.length&&j>= 0 && j < tablero[i].length) {
-            if(tablero[i][j]==jugador){
+            //i tiene que ser mayor o igal que 0 para no salirse del array
+            //a su vez no puede ser mayor que el array por el mismo motiva
+            //a la J le pasa igual pero dentro de su columna
+            if(tablero[i][j]==jugador){//VÁLIDO!!!
+                //si encuentra un jugador, volteará las fichas y se saldrá del bucle
                 voltear=true
-                
                 break
-            }else if(tablero[i][j]==null){
+            }else if(tablero[i][j]==null){//NO VÁLIDO!!!
+                //si está vacía la casilla, no volteará nada y saldrá del bucle
                 break
             }
             if(voltear==true){
                 tablero[i][j]=jugador
             }
+            //se van sumando lo que se tiene qeu sumar
             i+=suma_i
             j+=suma_j
         }
@@ -132,9 +148,11 @@ function volteaFicha(i,j,rival,jugador,suma_i,suma_j){
     }
     if(voltear==false){
         return false
+    }else{
+        return true
     }
 }
-//Función del juego***********************************************************************************
+//Función del juego***************************************************************************************************************************
 async function Juego() {
     let jugador=''//determina quien es el jugador en ese turno
     let rival=''//determina quien es el rivalen ese turno
